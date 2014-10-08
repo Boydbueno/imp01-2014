bd2c50d17a2a5d1730b502a281d6d3d2aa08c68c
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2014-10-08 12:16:43 +0200

    Expanded events examples

--- events/scripts/script.js
+++ events/scripts/script.js
@@ -2,11 +2,33 @@ var submitButton = document.getElementById("submit-button");
 
 submitButton.addEventListener("click", buttonClickHandler);
 
+var listItems = document.getElementsByTagName("li");
+
+for (var i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener("click", liClickHandler);
+}
+
+function liClickHandler(event) {
+    var clickedLi = event.target;
+    clickedLi.remove();
+    console.log(clickedLi);
+}
+
 function buttonClickHandler(event) {
     event.preventDefault();
-    var inputValue = document.getElementById("todo-input").value;
-    console.log(inputValue);
+    var inputElement = document.getElementById("todo-input");
+    var inputValue = inputElement.value;
 
-    // Todo: Add a new li to ul
-    // Hint: Use appendChild
+    // Todo: Create a li
+    var li = document.createElement("li");
+    // Todo: Fill the innerHTML of the li
+    li.innerHTML = inputValue;
+
+    // Todo: Add event listener to new list item
+    li.addEventListener("click", liClickHandler);
+    // Todo: append the li to the ul (appendChild)
+    var todoList = document.getElementById("todo");
+    todoList.appendChild(li);
+
+    inputElement.value = "";
 }
