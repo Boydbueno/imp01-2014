f79a7cdd5b752265e1eecbf26e2745f5f3fbd1b8
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2014-10-15 10:11:10 +0200

    Added basic html and javascript for shoppinglist oefening

---
+++ week7/oefening-shoppinglist/script.js
@@ -0,0 +1,14 @@
+var button = document.getElementById("submit");
+
+button.addEventListener("click", clickHandler);
+
+function clickHandler() {
+    var item = getData(); // ["shopping", "16-10-2014
+    console.log(item);
+    // Todo: Push items in a todos array
+    // Todo: Throw items in the DOM
+}
+
+function getData() {
+    // Todo: Functie aanvullen
+}
