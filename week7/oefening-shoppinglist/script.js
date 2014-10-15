8a446949085db886e22fbc602822be8715ca99da
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2014-10-15 10:50:26 +0200

    Completed todoList opdracht

--- week7/oefening-shoppinglist/script.js
+++ week7/oefening-shoppinglist/script.js
@@ -2,13 +2,55 @@ var button = document.getElementById("submit");
 
 button.addEventListener("click", clickHandler);
 
-function clickHandler() {
-    var item = getData(); // ["shopping", "16-10-2014
-    console.log(item);
-    // Todo: Push items in a todos array
+var todoItems = [];
+
+function clickHandler(e) {
+    e.preventDefault();
+    var todo = getData(); // ["shopping", "16-10-2014"]
+
+    // Push item in todos array
+    todoItems.push(todo);
+
     // Todo: Throw items in the DOM
+    // Table leegmaken
+    clearTable();
+
+    // Door alle items loopen
+    // Alle items in table plaatsen
+    addItemsToTable();
 }
 
 function getData() {
-    // Todo: Functie aanvullen
+    var item = document.getElementById("item").value; // item ophalen
+    if (item == "") {
+        var message = "Vul een omschrijving in!";
+        document.getElementById("error").innerHTML = message;
+    } else {
+        document.getElementById("error").innerHTML = "";
+    }
+
+    var date = document.getElementById("date").value; // datum ophalen
+    return [item, date]; // Als array returnen
+}
+
+function clearTable() {
+    var todoList = document.getElementById("todoList");
+    todoList.innerHTML = "";
+}
+
+function addItemsToTable() {
+    for (var i = 0; i < todoItems.length; i++) {
+        var item = todoItems[i];
+
+        var tr = document.createElement("tr");
+        var tdTodoDescription = document.createElement("td");
+        tdTodoDescription.innerHTML = item[0];
+        var tdTodoDate = document.createElement("td");
+        tdTodoDate.innerHTML = item[1];
+        tr.appendChild(tdTodoDescription);
+        tr.appendChild(tdTodoDate);
+
+        var todoList = document.getElementById("todoList");
+        todoList.appendChild(tr);
+    }
 }
